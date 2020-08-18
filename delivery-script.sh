01eec72a2651e27f7d4218c1673c51d49b83c487
mihanizm56 <m727507.56@mail.ru>
2020-08-18 21:18:56 +0300

    build(configure-package): added delivery script

---
+++ delivery-script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+PROJECT_PATH=$1
+
+BASE_PATH="${PROJECT_PATH}/node_modules/@wildberries/notifications"
+
+# build module
+npm run build
+
+# make filesystem operations
+rm -fr "${BASE_PATH}/dist"
+mkdir "${BASE_PATH}/dist"
+cp -avr dist "${BASE_PATH}"
+
+# go to the projetc folder
+cd $PROJECT_PATH
+
+# run the project
+# npx react-app-rewired start
