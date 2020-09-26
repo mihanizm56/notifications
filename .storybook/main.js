6ce69ada3f7846382900e67e082bb7fcccb50b05
mihanizm56 <m727507.56@mail.ru>
2020-09-27 01:12:12 +0300

    build(configure-package): update config

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,21 +3,14 @@
 const path = require('path');
 
 module.exports = {
-  stories: ['../stories/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ['../lib/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   webpackFinal: async config => {
-    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
-    // You can change the configuration based on that.
-    // 'PRODUCTION' is used when building the static version of storybook.
-
-    // Make whatever fine-grained changes you need
     config.module.rules.push({
-      test: /(module)?\.scss$/,
+      test: /(module)?\.s(a|c)ss$/,
       use: ['style-loader', 'css-loader', 'sass-loader'],
       include: path.resolve(__dirname, '../'),
     });
-
-    // Return the altered config
 
     config.resolve = {
       ...config.resolve,
